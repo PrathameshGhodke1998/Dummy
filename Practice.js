11bbe53189ae18ce6b31842d73d7bd07cfa38931
Prathamesh <prathamesh@demo.com>
2024-04-24 00:43:13 +0530

    "first commiting practice file"

---
+++ Practice.js
@@ -0,0 +1,24 @@
+
+let student = {
+    name : 'prem',
+    id : 1,
+    subjects : {
+        hindi : 50,
+        english : 80
+    }
+}
+
+for(let key in student){
+console.log(student[key])
+}
+
+let mark = 100
+console.log(`total marks: ${mark}`)
+
+let a = undefined
+let b = 1
+
+console.log(a+b)
+console.log(typeof(NaN))
+
+
